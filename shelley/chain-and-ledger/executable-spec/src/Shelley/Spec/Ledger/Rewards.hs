16a3007b0c34964b98aa58a9fdca98f948ec58a5
Jared Corduan <jared.corduan@iohk.io>
2021-07-21 10:32:11 -0400

    at major PV 5, script stake creds can earn rewards

--- shelley/chain-and-ledger/executable-spec/src/Shelley/Spec/Ledger/Rewards.hs
+++ shelley/chain-and-ledger/executable-spec/src/Shelley/Spec/Ledger/Rewards.hs
@@ -465,7 +465,7 @@ aggregateRewards pp rewards =
 rewardOnePool ::
   forall c m.
   Monad m =>
-  (UnitInterval, NonNegativeInterval, Natural) ->
+  (UnitInterval, NonNegativeInterval, Natural, Natural) ->
   Coin ->
   Natural ->
   Natural ->
@@ -480,7 +480,8 @@ rewardOnePool ::
     m
     (Map (Credential 'Staking c) (Set (Reward c)))
 rewardOnePool
-  (pp_d, pp_a0, pp_nOpt) -- (Decentralization parameter, Pool influence, Desired number of pools)
+  (pp_d, pp_a0, pp_nOpt, pp_mv)
+  -- (Decentralization parameter, Pool influence, Desired number of pools, major version)
   r
   blocksN
   blocksTotal
@@ -524,7 +525,7 @@ rewardOnePool
               hk `Set.member` addrsRew
           ]
       notPoolOwner (KeyHashObj hk) = hk `Set.notMember` _poolOwners pool
-      notPoolOwner (ScriptHashObj _) = False
+      notPoolOwner (ScriptHashObj _) = HardForks.allowScriptStakeCredsToEarnRewards pp_mv
       lReward =
         leaderRew
           poolR
--- shelley/chain-and-ledger/shelley-spec-ledger-test/src/Test/Shelley/Spec/Ledger/Serialisation/EraIndepGenerators.hs
+++ shelley/chain-and-ledger/shelley-spec-ledger-test/src/Test/Shelley/Spec/Ledger/Serialisation/EraIndepGenerators.hs
@@ -994,6 +994,7 @@ instance
       <*> arbitrary {- pp_d -}
       <*> arbitrary {- pp_a0 -}
       <*> arbitrary {- pp_nOpt -}
+      <*> arbitrary {- pp_mv -}
 
 instance
   Mock crypto =>
